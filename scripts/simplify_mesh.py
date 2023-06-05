fca818d5c4c37bea46f714cbac541bfb4e21e739
RuiqiW <37905856+RuiqiW@users.noreply.github.com>
2023-06-05 13:47:35 -0700

    init

---
+++ scripts/simplify_mesh.py
@@ -0,0 +1,73 @@
+import open3d as o3d
+import os
+import numpy as np
+import trimesh
+
+
+
+
+# RAND = True
+PREFIX = 'train'
+mesh_dir = '../{}_meshMNIST/test/'.format(PREFIX)
+output_dir = '../{}_mesh/val/'.format(PREFIX)
+# rot_mat_dir = '../data/{}_rotations'.format(PREFIX)
+
+
+idx = 0
+# for filename in ['00028.obj']:
+
+label_file = os.path.join('../train_meshMNIST', 'labels.txt')
+f = open(label_file, 'r')
+lines = f.readlines()
+
+count = np.zeros(10)
+
+
+for filename in os.listdir(mesh_dir):
+    name, suffix = filename.split('.')
+    
+    if idx % 1000 == 0:
+        print(idx)
+        print(count)
+
+    if suffix != 'obj':
+        continue  
+
+    f = os.path.join(mesh_dir, filename)
+
+    # mesh = trimesh.load(f)
+    # F = np.array(mesh.faces).shape[0]
+    # print(F)
+    # faces = trimesh.repair.broken_faces(mesh)
+    # print(len(faces))
+    # face_mask = np.ones(F, dtype=bool)
+    # for i in faces:
+    #     face_mask[i] = False
+    # mesh.update_faces(face_mask)
+
+    # print(np.array(mesh.faces).shape[0])
+    # trimesh.repair.fill_holes(mesh)
+    # # print(mesh.is_watertight)
+
+
+    mesh = o3d.io.read_triangle_mesh(f)
+    mesh_smp = mesh.simplify_quadric_decimation(target_number_of_triangles=480)
+    mesh_smp_2 = mesh.simplify_quadric_decimation(target_number_of_triangles=180)
+    if not mesh_smp.is_watertight() or not mesh_smp_2.is_watertight():
+        id = int(name)
+        label = int(lines[id-1].split(',')[1])
+        count[label] += 1
+        continue
+
+    # # if not mesh_smp.is_watertight():
+    # #     print(idx)
+    # #     continue
+    # # print(
+    # #     f'Simplified mesh has {len(mesh_smp.vertices)} vertices and {len(mesh_smp.triangles)} triangles'
+    # # )
+    # # o3d.visualization.draw_geometries([mesh_smp])
+    o3d.io.write_triangle_mesh(os.path.join(output_dir, name + '.obj'), mesh_smp)
+
+    idx += 1
+
+print(count)
